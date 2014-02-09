3370dbf46b6905559aae3da6add6e6c5de24d9d8
shipperizer <alexcabb@gmail.com>
2014-02-09 19:55:31 +0000

    q3 solved: seems working

--- test-questions-master/q3/debug.php
+++ test-questions-master/q3/debug.php
@@ -6,26 +6,25 @@ trait FileHandler {
     }
 }
 
-class Logger implements FileHandler {
-        
-    $log;
-    private $file;
+class Logger {
+    
+    use FileHandler;
+    private static $file;
     
     public function Logger($file) {
-        
         // Constructs the Logger
         self::$file = fopen($file, "write+");
     }
     
-    final private function write-To-Log($string) {
+    final public function write_To_Log($string) {
         
         // Writes the new line to log and then makes sure that the connection is still open.
-        fwrite($self::$file, $string."<br />");
+        fwrite(self::$file, $string."<br />");
         self::$file = fopen(self::$file, "write+");
     }
     
     function printLog() {
-        while ($line = fread($self::$file)) {
+        while ($line = fread(self::$file)) {
             echo $line."\n";
         }
     }
@@ -36,10 +35,10 @@ $Logger = new Logger("/etc/log/myfile.log");
 
 $x = 1;
 for ( $x; $x<1000; $x++ ) {
-    $Logger->{"write-To-Log"}( "x is ",$x );
+    $Logger->{"write_To_Log"}( "x is ",$x );
 }
 
-echo printLog();
+echo $Logger->printLog();
 
 
 ?>
